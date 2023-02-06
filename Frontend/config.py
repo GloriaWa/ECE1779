ed1c9507d51334b740e90be914f64d2953890043
GloriaWa <h536wang@uwaterloo.ca>
2023-02-05 19:54:27 -0500

    db config

--- Frontend/config.py
+++ Frontend/config.py
@@ -1,6 +1,6 @@
 db_config = {'user': 'root',
              'password': 'Mypass123!',
-             'host': '127.0.0.1',
+             'host': 'a1.cgnq4uzclidq.us-east-2.rds.amazonaws.com',
              'database': 'a1'}
 
 capacity = 12
